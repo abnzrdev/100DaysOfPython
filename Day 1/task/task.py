4002211746d8b1ff205ed5bdd9eab8a243274d1c
Abenezer <abnzr.dev@gmail.com>
2025-06-08 23:02:51 +0500

    Add all the files

---
+++ Day 1/task/task.py
@@ -0,0 +1,4 @@
+print("Hello, Welcome to the program")
+city = input("Which city were you grow up?\n")
+petName = input("what is your pet name\n")
+print("Your band name can be " + city + " " + petName)
