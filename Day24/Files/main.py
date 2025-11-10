0f1a8645994470f0e8ecb5c9b8e1004f2d9ec7f2
Abenezer <abnzr.dev@gmail.com>
2025-11-11 00:27:21 +0500

    Add file handling for high score in Scoreboard; create data.txt for persistence

---
+++ Day24/Files/main.py
@@ -0,0 +1,5 @@
+file = open("hello.txt")
+content = file.read()
+print(content)
+file.close()   # it is needed to close just to free up the resources manually using this lines code
+
--- Day24/SnakeGameModified/scoreboard.py
+++ Day24/SnakeGameModified/scoreboard.py
@@ -2,13 +2,21 @@ from turtle import Turtle
 ALIGNMENT = "center"
 FONT = ("Courier", 24, "normal")
 
+#Open the file
+
+
+
 
 class Scoreboard(Turtle):
 
     def __init__(self):
         super().__init__()
         self.score = 0
-        self.highscore = 0
+        try:
+            with open("data.txt", "r") as data:
+                self.highscore = int(data.read())
+        except FileNotFoundError:
+            self.highscore = 0
         self.color("white")
         self.penup()
         self.goto(0, 270)
@@ -23,6 +31,8 @@ class Scoreboard(Turtle):
     def reset(self):
         if self.score > self.highscore:
             self.highscore = self.score
+            with open("data.txt", "w") as data:
+                data.write(str(self.score))
         self.score = 0
         self.update_scoreboard()
 
